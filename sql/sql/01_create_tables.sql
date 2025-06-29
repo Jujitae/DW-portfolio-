1fa582f7179fd0645bafb3aa69b9a5a4ddbb894b
Jujitae <dldpcks7892@gmail.com>
2025-06-29 21:19:56 +0900

    feat(sql): add base customer and order table schema

--- sql/sql/01_create_tables.sql
+++ sql/sql/01_create_tables.sql
@@ -1,14 +1,15 @@
--- [기초 테이블 생성]
+-- [고객 테이블]
 CREATE TABLE customers (
   customers_id   NUMBER PRIMARY KEY,
   customers_name VARCHAR2(100),
   signup_date    DATE
   );
 
+-- [주문 테이블]
 CREATE TABLE orders (
   order_id  NUMBER PRIMARY KEY,
   customer_id NUMBER,
   order_date DATE,
   amount NUMBER,
-  FOREIGN KEY (customer_id)  REFERENCES customer(customer_id)
+  FOREIGN KEY (customer_id)  REFERENCES customers(customer_id)
   );
